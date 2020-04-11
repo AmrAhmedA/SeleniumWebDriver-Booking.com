c032427a5ded9cb3391dd19c5022c34b6b06b582
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-04-11 04:07:07 +0200

    Adding Subscribe Negative Testcase

--- src/Testcases/Functionalities.java
+++ src/Testcases/Functionalities.java
@@ -185,4 +185,47 @@ public class Functionalities {
         driver.close();
         driver.quit();
     }
+    
+    
+     public void SubscribeToBookingNegative() throws InterruptedException {
+
+        // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Amr\\Desktop\\chromedriver.exe");
+
+        // Instantiate a ChromeDriver class to establish a connection       
+        WebDriver driver = new ChromeDriver();
+
+        //Puts an implicit wait, will wait for 10 seconds before throwing exception
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Launch Website
+        driver.navigate().to("https://www.booking.com/");
+
+        //Maximize the Browser
+        driver.manage().window().maximize();
+
+        //Type Casting driver to scroll inside the page for the Subscribe Element
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        //simple js code scrolling 1500 dpi
+        js.executeScript("scrollBy(0,1500)");
+
+        //Entering invalid email address
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/div[2]/div[3]/div[5]/div/div/div/div[2]/div[2]/form/div/input")).sendKeys("123@123");
+
+        //clicking on the subscribe button
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/div[2]/div[3]/div[5]/div/div/div/div[2]/div[2]/form/div/button/span")).click();
+
+        //Check Element not present, after subscribing to booking services
+        if (driver.findElement(By.xpath("//*[@id=\"emk_banner_index\"]/div[2]/div[2]/form/p[1]")).isDisplayed()) {
+            System.out.println("Element is Present");
+        } else {
+            System.out.println("Element is Absent");
+        }
+        
+
+        //Closing Streams
+        driver.close();
+        driver.quit();
+    }
 }
