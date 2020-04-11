cce478f7b904a2ddf7bb6fda4dbb884e139df672
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-04-11 14:52:53 +0200

    Adding search for flights

--- src/BookingMain/Main.java
+++ src/BookingMain/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         
         Functionalities b1 = new Functionalities();
-        b1.SearchforFlightsNegative();
+        b1.SearchforFlights();
     }
 
 }
