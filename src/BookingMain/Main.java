c2e1f606de82b03898c641a2e072ca4e89bd038f
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-04-11 18:04:32 +0200

    Removing comments

--- src/BookingMain/Main.java
+++ src/BookingMain/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         
+        //remove the comment to run each one individually 
         Functionalities b1 = new Functionalities();
         b1.BookingSignin();
 //        b1.BookingSignout();
@@ -32,30 +33,4 @@ public class Main {
 /**
  *
  ********AmrAhmed-162697********
- */
-
-/*
-		//Click on Percent Calculators
-		driver.findElement(By.xpath("//*[@id=\"content\"]/table[2]/tbody/tr/td/div[3]/a")).click();
-		
-		//Enter Value 10 in the First Number of the Percent Calculator
-		driver.findElement(By.id("cpar1")).sendKeys("10");
-		
-		//Enter Value 50 in the Second Number of the Percent Calculator
-		driver.findElement(By.id("cpar2")).sendKeys("50");
-		
-		//Click Calculate Button
-		driver.findElement(By.xpath("//*[@id=\"content\"]/table[1]/tbody/tr[2]/td/input[2]")).click();
-		
-		//Get the Result Text based on its XPath
-		String result=	driver.findElement(By.xpath("//*[@id=\"content\"]/h2[1]")).getText();
-		
-		//Check if actual result matches expected result
-		
-		//Print Success Message to Console
-		if(result==exp){
-			System.out.println("Correct!");
-		}else{
-			System.out.println("Incorrect answer!");
-		}
  */
